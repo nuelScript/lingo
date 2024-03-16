ef6cf6e836ac629eb001805a3528e7de7bff3dd4
Parseval <igwee3333@gmail.com>
2024-03-16 20:57:10 +0100

    chore: added desktop and mobile sidebar

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+type Props = {
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+export const SidebarItem = ({ href, iconSrc, label }: Props) => {
+  const pathname = usePathname();
+  const active = pathname === href;
+  return (
+    <Button
+      variant={active ? "sidebarOutline" : "sidebar"}
+      className="justify-start h-[52px]"
+      asChild
+    >
+      <Link href={href}>
+        <Image
+          src={iconSrc}
+          alt={label}
+          className="mr-5"
+          height={32}
+          width={32}
+        />
+        {label}
+      </Link>
+    </Button>
+  );
+};
