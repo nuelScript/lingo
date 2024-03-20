2c0deefe9ef7c8f583dadb0dd2c19db9b6652f98
Parseval <igwee3333@gmail.com>
2024-03-20 21:35:02 +0100

    chore: dynamic entries

---
+++ scripts/seed.ts
@@ -0,0 +1,53 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+// @ts-ignore
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding the database");
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+      {
+        id: 3,
+        title: "Croatian",
+        imageSrc: "/hr.svg",
+      },
+      {
+        id: 4,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 5,
+        title: "Japanese",
+        imageSrc: "/jp.svg",
+      },
+    ]);
+
+    console.log("Seeded the database");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed the database");
+  }
+};
+
+main();
