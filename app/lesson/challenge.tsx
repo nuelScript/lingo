7c32ad661f266aee4d621acbfee4a73a9588154c
Parseval <igwee3333@gmail.com>
2024-05-03 12:29:46 +0100

    chore: added card and challenges

---
+++ app/lesson/challenge.tsx
@@ -0,0 +1,48 @@
+import { challengeOptions, challenges } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import { Card } from "./card";
+
+type Props = {
+  options: (typeof challengeOptions.$inferSelect)[];
+  onSelect: (id: number) => void;
+  status: "correct" | "wrong" | "none";
+  selectedOption?: number;
+  disabled: boolean;
+  type: (typeof challenges.$inferSelect)["type"];
+};
+
+export const Challenge = ({
+  disabled,
+  onSelect,
+  options,
+  status,
+  type,
+  selectedOption,
+}: Props) => {
+  return (
+    <div
+      className={cn(
+        "grid gap-2",
+        type === "ASSIST" && "grid-cols-1",
+        type === "SELECT" &&
+          "grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]"
+      )}
+    >
+      {options.map((option, i) => (
+        <Card
+          key={option.id}
+          id={option.id}
+          imageSrc={option.imageSrc}
+          text={option.text}
+          shortcut={`${i + 1}`}
+          selected={selectedOption === option.id}
+          onClick={() => onSelect(option.id)}
+          status={status}
+          audioSrc={option.audioSrc}
+          disabled={disabled}
+          type={type}
+        />
+      ))}
+    </div>
+  );
+};
--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,8 @@
 import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
+import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
 
 type Props = {
   initialPercentage: number;
@@ -24,6 +26,21 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts);
   const [percentage, setPercentage] = useState(initialPercentage);
+  const [challenges, setChallenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    );
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex;
+  });
+
+  const challenge = challenges[activeIndex];
+  const options = challenge?.challengeOptions ?? [];
+
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
   return (
     <div>
       <>
@@ -32,6 +49,28 @@ export const Quiz = ({
           percentage={percentage}
           hasActiveSubscription={!!userSubscription?.isActive}
         />
+        <div className="flex-1">
+          <div className="h-screen flex items-center justify-center">
+            <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+              <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+                {title}
+              </h1>
+              <div className="">
+                {challenge.type === "ASSIST" && (
+                  <QuestionBubble question={challenge.question} />
+                )}
+                <Challenge
+                  options={options}
+                  onSelect={() => {}}
+                  status="none"
+                  selectedOption={undefined}
+                  disabled={false}
+                  type={challenge.type}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     </div>
   );
